c53fd1ffed7d791d960f49740cc412071020d559
Fernando Yurisich <fyurisich@oohg.org>
2019-08-07 18:06:06 -0300

    Error in Path fixed
    2019-08-07 18:05 UTC-0300 Fernando Yurisich <fyurisich@oohg.org>
       * samples\ado\Build.bat
       * samples\dll\LANZA.BAT
       * samples\mysql\demobuild.bat
         ! Path is not properly restored if it contains & chars.

--- mysql/demobuild.bat
+++ mysql/demobuild.bat
@@ -1,9 +1,9 @@
 @echo off
 if "%HG_ROOT%"="" set HG_ROOT=c:\oohg
 if not exist %HG_ROOT%\hb32\lib\LibDllMySql_O3GH.a copy LibDllMySql_O3GH.a %HG_ROOT%\hb32\lib
-set TPATH=%PATH%
-set PATH=%HG_ROOT%
+set "TPATH=%PATH%"
+set "PATH=%HG_ROOT%"
 set THR_LIB=-lhbmysql -lDllMySql_O3GH
 call COMPILE.BAT demo
-set PATH=%TPATH%
+set "PATH=%TPATH%"
 set THR_LIB=
