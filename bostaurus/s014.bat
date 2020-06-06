399bd8452c5597c57f5aaf5e93ea10682744e660
Fernando Yurisich <fyurisich@oohg.org>
2020-06-06 00:57:15 -0300

    Use new compile.bar features
    2020-05-06 00:56 UTC-0300 Fernando Yurisich <fyurisich@oohg.org>
       * samples\bostaurus\s014.bat
         * Use new compile.bat features.

--- bostaurus/s014.bat
+++ bostaurus/s014.bat
@@ -1,11 +1,14 @@
 @echo off
 
-if "%HG_ROOT%"=="" set HG_ROOT=c:\oohg
+setlocal
+
+if "%HG_ROOT%"=="" set HG_ROOT=C:\OOHG
+if "%HG_HRB%"=="" set HG_HRB=%HG_ROOT%\HB32
 
 REM If you want to use hbmk2 to build the exe, uncomment the following line and comment all others:
 REM BuildApp s014 -i%HG_ROOT%\hb32\contrib\hbzebra
 
 REM If you want to use harbour and gcc to build the exe, uncomment the following line and comment all others:
-REM COMPILE s014 -i%HG_ROOT%\hb32\contrib\hbzebra
+REM COMPILE s014
 
-COMPILE s014 -i%HG_ROOT%\hb32\contrib\hbzebra
+COMPILE s014
