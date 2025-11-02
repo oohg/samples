a8411c7933a7b49af6bbff3c4a1b61cd7ddfc17d
Fernando Yurisich <fyurisich@users.sourceforge.net>
2025-11-02 20:50:53 -0300

    Preserve path

--- mysql/demobuild.bat
+++ mysql/demobuild.bat
@@ -2,7 +2,7 @@
 if "%HG_ROOT%"=="" set HG_ROOT=c:\oohg
 if not exist %HG_ROOT%\hb32\lib\LibDllMySql_O3GH.a copy LibDllMySql_O3GH.a %HG_ROOT%\hb32\lib
 set "TPATH=%PATH%"
-set "PATH=%HG_ROOT%"
+set "PATH=%HG_ROOT%;%PATH%"
 set HG_ADDLIBS_TEMP=%HG_ADDLIBS%
 set HG_ADDLIBS=%HG_ADDLIBS% -lhbmysql -lDllMySql_O3GH
 call COMPILE.BAT demo
