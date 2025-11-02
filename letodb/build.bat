66c4b7a9c466a2e2762a628005abb7c26d6af969
Fernando Yurisich <fyurisich@users.sourceforge.net>
2025-11-02 20:50:33 -0300

    Preserve path

--- letodb/build.bat
+++ letodb/build.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%HG_ROOT%"="" set HG_ROOT=c:\oohg
 set "TPATH=%PATH%"
-set "PATH=%HG_ROOT%"
+set "PATH=%HG_ROOT%;%PATH%"
 set HG_ADDLIBS_TEMP=%HG_ADDLIBS%
 set HG_ADDLIBS=%HG_ADDLIBS% -lrddleto
 call COMPILE.BAT demo
